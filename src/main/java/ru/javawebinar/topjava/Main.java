ff03a8eca6bcfda8cf881f07186ece3013643e31
IYgit <novemb75@gmail.com>
2018-02-23 15:48:24 +0200

    посилання

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -8,7 +8,8 @@ package ru.javawebinar.topjava;
  * @link https://github.com/JavaOPs/topjava
  */
 /* java 8: streams: https://www.tutorialspoint.com/java8/java8_streams.htm
-* http://www.baeldung.com/java-8-streams*/
+* http://www.baeldung.com/java-8-streams
+* https://habrahabr.ru/post/348536/*/
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
